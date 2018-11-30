c7c7b69c2ae7f4fee4c236aab41c27a05a5f2186
tomkeith <tomah5714@gmail.com>
2018-11-30 10:55:08 +0300

    [finishes #162336517] user deletes specific redflag

--- app/api/v1/redflags/models.py
+++ app/api/v1/redflags/models.py
@@ -34,5 +34,83 @@ class RedFlagsModels():
 	def get_all(self):
 		return self.db
 
+	def edit_location(self, num):
+		for redflag in self.db:
+			if redflag['id'] == int(num):
+				if redflag ['status'] == 'draft':
+					redflag.update(request.get_json())
+					return{
+						'status':200,
+						'data':[{
+							'id': num,
+							'message':'updated redflag location'
+						}]
+					}
+				elif redflag['status'] == 'under investigation':
+					return{
+						'status':404,
+						'message': 'redflag under investigation'
+					}
+				elif redflag['status'] == 'Resolved':
+					return{
+						'status':404,
+						'message': 'redflag Resolved'
+					}
+				elif redflag['status'] == 'Rejected':
+					return{
+						'status':404,
+						'message': 'redflag Rejected'
+					}
+
+	def edit_comment(self, num):
+		for redflag in self.db:
+			if redflag['id'] == int(num):
+				if redflag ['status'] == 'draft':
+					redflag.update(request.get_json())
+					return{
+						'status':200,
+						'data':[{
+							'id': num,
+							'message':'updated redflag comment'
+						}]
+					}
+				elif redflag['status'] == 'under investigation':
+					return{
+						'status':404,
+						'message': 'redflag under investigation'
+					}
+				elif redflag['status'] == 'Resolved':
+					return{
+						'status':404,
+						'message': 'redflag Resolved'
+					}
+				elif redflag['status'] == 'Rejected':
+					return{
+						'status':404,
+						'message': 'redflag Rejected'
+					}
+
+	def get_one(self, num):
+		for redflag in self.db:
+			if redflag['id'] == num:
+				return redflag
+			else:
+				return "No content found"
+
+	def put_one(self, num):
+		for redflag in self.db:
+			if redflag['id'] == num:
+				return redflag
+			else:
+				return "No content found"
+
+	def destroy(self, num):
+		for redflag in redflags:
+			if redflag['id'] == num:
+				redflags.pop(num - 1)
+				return "Deleted successfully"
+			else:
+				return "No content found"
+
 	
 		
